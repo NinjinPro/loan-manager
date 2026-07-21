673fb86a47db2c1882a668e61957e9dea326e2c5
ninjinpro <gracieuxdevelop@gmail.com>
2026-07-21 17:57:27 +0200

    v2: made reqdme visible, made run.py quite dynamic hostable

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     host = False"192.168.43.226"
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
