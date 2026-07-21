2e745a7b1feac0eb288591d5eb811f028b6bd75d
ninjinpro <gracieuxdevelop@gmail.com>
2026-07-21 11:45:40 +0200

    modified alembic + app-config to focus on .env configuration

--- alembic/env.py
+++ alembic/env.py
@@ -18,13 +18,22 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from db.database import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+from dotenv import load_dotenv
+load_dotenv()
+url = os.getenv("DATABASE_URI", None)
+
+if not url:
+    raise ValueError("Loading DATABASE_URI from .env didn't work")
+    
+context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- app/config.py
+++ app/config.py
@@ -1,20 +1,18 @@
-# app/config.py
 import os
-from dotenv import loadenv
+from dotenv import load_dotenv
 
-loadenv()
+load_dotenv()
+
 
 class Config:
-    SECRET_KEY: str = os.environ.get("SECRET_KEY", "dev-secret-change-me")
-    SQLALCHEMY_DATABASE_URI: str = os.environ.get(
-        "DATABASE_URL", "sqlite:///loan_manager.db"
-    )
-    SQLALCHEMY_ENGINE_OPTIONS: dict = {
-                "pool_size": 10,
-                "pool_recycle": 3600,
-                "pool_pre_ping": True,
-            }
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-    #def __post_init__(self):
-        #if not self.SQLALCHEMY_ENGINE_OPTIONS:
-            #self.SQLALCHEMY_ENGINE_OPTIONS = 
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+    )
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_size": 10,
+        "pool_recycle": 3600,
+        "pool_pre_ping": True,
+    }
